c55f5e366e385c330282f8370232c5db7b7b10fe
jjtroiano0272 <jjtroiano0272@eagle.fgcu.edu>
2018-10-29 12:05:33 -0400

    Monday work

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,15 +8,22 @@ import javafx.fxml.FXML;
 public class Controller {
 	
 	@FXML
-	private TextField inputC;
+	private double inputC;
 	
 	@FXML
 	private Label outputF;
 	
 	@FXML
 	void convertTemp(ActionEvent event) {
-		double degreeesC = Double.parseDouble(inputC);
-		double degreeesF = degreeesC * (9/5) + 32;
+		double degreesF = Double.parseDouble(inputC.getText());
+		double degreeesF = degreeesC * (9 / 5) + 32;
 		outputF.setText(Double.toString(degreeesF));
-	};
+	}
+	
+	// You'll need interface ChangeListener.
+	// You can use anonymous interface implementation for this interface OR a lambda exp!
+	
+	public void initalize() {
+		btnConvert.setDefaultButton(true);
+	}
 }
