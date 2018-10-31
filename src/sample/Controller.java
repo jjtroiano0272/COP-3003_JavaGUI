d33da2c5a7b6f4f1e9b6feaa4d4f6ae28fc4a175
jjtroiano0272 <jtroianofau@gmil.com>
2018-10-31 16:39:01 -0400

    Changes sample.fxml to TipCalculator.fxml and sets up the whole scene properly in JavaFX scene builder. I'm fucking starving and gotta get outta here!!!

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,9 +3,22 @@ package sample;
 import java.awt.Label;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Slider;
+import javafx.stage.Stage;
+import javax.swing.plaf.SliderUI;
+import javax.xml.soap.Text;
 
 public class Controller {
+	
+	/* Commented out in order to work on sandbox version of GUI...
+	
 	
 	@FXML
 	private double inputC;
@@ -26,4 +39,26 @@ public class Controller {
 	public void initalize() {
 		btnConvert.setDefaultButton(true);
 	}
+	*/
+	
+	private static final NumberFormat currency = NumberFormat.getCurrencyInstance();
+	private static final NumberFormat percent = NumberFormat.getPercentInstance();
+	
+	private BigDecimal tipPercentage = new BigDecimal(0.20);
+	
+	@FXML
+	private TextField boxAmount;
+	
+	@FXML
+	private Label lblPercentage;
+	
+	@FXML
+	private Slider sldPercentage;
+	
+	@FXML
+	private TextField boxTip;
+	
+	@FXML
+	private TextField lblTotal;
+	
 }
